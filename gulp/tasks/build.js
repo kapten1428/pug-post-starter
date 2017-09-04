4d862eccac4f95427766b34e591eceb7fc8b5e29
kapten1428 <kapten1428@gmail.com>
2017-09-04 21:29:10 +0700

    ready to build

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,2 +1,35 @@
 let gulp = require('gulp'),
+usemin = require('gulp-usemin'),
+cssnano = require('gulp-cssnano'),
 browserSync = require('browser-sync').create();
+
+gulp.task('testDocs',() => {
+	browserSync.init({
+		notify: false,
+		server:{
+			baseDir : "docs"
+		}
+	})
+});
+
+gulp.task('createDocs',()=>{
+	let copyFolder = [
+	'./app/**/*',
+	'!./app/assets/styles/**/*',
+	'!./app/pug/**/*',
+	'!./app/temp',
+	'!./app/temp/**/*',
+	]
+	return gulp.src(copyFolder)
+	.pipe(gulp.dest('./docs'))
+});
+
+gulp.task('usemin',['styles'],()=>{
+	return gulp.src("./app/index.html")
+	.pipe(usemin({
+		css:[function() {return cssnano()}],
+	}))
+	.pipe(gulp.dest("./docs"));
+});
+
+gulp.task('build',['createDocs','usemin']);
